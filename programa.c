a9c704f40cd2dde0077e901c64147e9a8a9b8d69
Matheus Sarno Muiños <matheusmuinos@gmail.com>
2025-11-26 09:55:15 +0100

    version final

--- programa.c
+++ programa.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    printf("Hola mundo\n");
+    printf("Cambio de imagen hecho.\n");
+    printf("Pausa para mirar el mapa de memoria después del cambio de imagen\n");
+    scanf("%*d");
+    getchar();
     return 0;
 
 }
